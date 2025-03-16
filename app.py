8cf148ff9cd4598b4996a78dca2bf530c0ab47c5
trietnguyen0711 <nguyenminhtriet07112008@gmail.com>
2025-03-16 20:40:07 +0700

    Test

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-import streamlit as st
+import hehe as st
 import numpy as np
 import pandas as pd
-df = pd.read_csv("score.csv")
-print(df)
+def read_data():
+    return pd.read_csv("score.csv")
+def show_data():
+    st.dataframe(read_data(),hide_index=True)
+    
+def main():
+    df = read_data()
+    show_data(df)
+main()
